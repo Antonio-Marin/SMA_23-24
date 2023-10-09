7bcadf365c3cbde7c157550e91b2fd1a10090d35
PabloRozalen01 <pablooboe@hotmail.es>
2023-10-09 12:01:40 +0200

    Creacion base del menú

--- base-P3/src/Main.java
+++ base-P3/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+    import java.util.Scanner;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            int op;
+            System.out.println("Introduce una opción: \n 1. Crear nuevo agente\n 2. Crear Descendiente\n 3. Mandar Mensaje\n 4. Recibir mensaje");
+            op = s.nextInt();
+            switch(op){
+                case 1:
+                    System.out.println("Opcion 1");
+                    break;
+                case 2:
+                    System.out.println("Opcion 2");
+                    break;
+                case 3:
+                    System.out.println("Opcion 2");
+                    break;
+                case 4:
+                    System.out.println("Opcion 2");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecto");
+                    break;
+            }
+        }
     }
 }
